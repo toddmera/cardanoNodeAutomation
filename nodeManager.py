9f38608ba648cd74ba9eca366ccf00d39b1bd4b8
Todd Mera <tkmera@gmail.com>
2020-10-20 16:06:55 -0700

    .

--- nodeManager.py
+++ nodeManager.py
@@ -1,11 +1,17 @@
 import env
 
+# Main menu where it all starts
 def show_main_menu():
-    print("1: Option 1")
+    print("1: Wallet(s)")
     print("2: Option 2")
     print("3: Option 3")
     print("4: Option 4")
     print("0: Exit")
+
+def show_wallet_menu():
+    print("1: Option 1")
+    print("2: Option 2")
+    print("0: Main Menu")
 
 show_main_menu()
 option = int(input("Enter your option:"))
@@ -14,7 +20,15 @@ while option != 0:
     if option == 1:
         # do stuff here
         print("You selected option 1")
-        print("Timeout is: {}".format(env.timeout))
+        show_wallet_menu()
+        option = int(input("Enter your option:"))
+        while option != 0:
+            if option == 1:
+                print("You selected option 1")
+            elif option == 2:
+                print("You selected Option 2")
+            else:
+                print("Invalid option")
     elif option == 2:
         # do stuff here
         print("You selected option 2")
